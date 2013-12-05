a86dfab1af8b78f720843e3346f1d2a721bedcda
Ery <ery@baoleihang.com>
2013-12-06 04:28:23 +0800

    remove json

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -42,45 +42,36 @@ class TopicsController < ApplicationController
   def create
     @topic = Topic.new(topic_params)
 
-    respond_to do |format|
-      if @topic.save
-        format.html { redirect_to @topic, notice: 'Topic was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @topic }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @topic.errors, status: :unprocessable_entity }
-      end
+    if @topic.save
+      redirect_to @topic, notice: 'Topic was successfully created.'
+    else
+      render action: 'new'
     end
   end
 
   def update
-    respond_to do |format|
-      if @topic.update(topic_params)
-        format.html { redirect_to @topic, notice: 'Topic was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @topic.errors, status: :unprocessable_entity }
-      end
+    if @topic.update(topic_params)
+      redirect_to @topic, notice: 'Topic was successfully updated.'
+    else
+      render action: 'edit'
     end
   end
 
   def destroy
     @topic.destroy
-    respond_to do |format|
-      format.html { redirect_to topics_url }
-      format.json { head :no_content }
-    end
+    redirect_to topics_url
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_topic
-      @topic = Topic.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def topic_params
-      params.require(:topic).permit(:title, :url, :memo)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_topic
+    @topic = Topic.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def topic_params
+    params.require(:topic).permit(:title, :url, :memo)
+  end
+
 end
--- app/views/topics/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @topic, :id, :title, :url, :memo, :created_at, :updated_at
