0289ef22629dd600de01f03bdc6c2296b945bf11
Ery <ery@baoleihang.com>
2013-12-06 05:22:34 +0800

    action header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def draw_action_header(header = nil)
+    header ||= "#{controller_name}.#{action_name}".titleize
+    render 'layouts/action_header', :header => header
+  end
 end
