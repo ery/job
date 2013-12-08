289cfd0bd4b30c4eb0b836d888fbd772b7cdcdbd
Ery <ery@baoleihang.com>
2013-12-08 15:38:07 +0800

    No message.

--- db/migrate/20131207193046_create_topics.rb
+++ db/migrate/20131207193046_create_topics.rb
@@ -2,11 +2,8 @@ class CreateTopics < ActiveRecord::Migration
   def change
     create_table :topics do |t|
       t.string :title
-      t.string :orginal_id
       t.string :url
-      t.string :author
-      t.string :author_url
-      t.string :local_file
+      t.string :file
       t.timestamps
     end
   end
