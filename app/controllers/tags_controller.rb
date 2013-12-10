d41430375f1ff19a8f86fc5f0442057dba1f7667
Ery <ery@baoleihang.com>
2013-12-11 02:26:11 +0800

    No message.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,5 +4,39 @@ class TagsController < ApplicationController
     @tags = Tag.all
   end
 
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      redirect_to @tag, notice: '新建成功.'
+    else
+      render action: 'new'
+    end
+  end
+
+  def edit
+    @tag = Tag.find(params[:id])
+  end
+
+  def update
+    @tag = Tag.find(params[:id])
+    if @tag.update(tag_params)
+      redirect_to @tag, notice: '修改成功.'
+    else
+      render action: 'edit'
+    end
+  end
+
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
+
 end
 
