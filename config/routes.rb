7801633f66cee3321fb2c4c65824f0685e186c67
Ery <ery@baoleihang.com>
2013-12-11 17:24:41 +0800

    No message.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Job::Application.routes.draw do
   resources :topics do
+    put :tag,     :on => :member, :action => :tag
+
     put :inbox,   :on => :member, :action => :put_inbox
     put :ignored, :on => :member, :action => :put_ignored
     put :cared,   :on => :member, :action => :put_cared
