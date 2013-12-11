da22d2602e2e414ba5253d24169ed981bd42e15c
Ery <ery@baoleihang.com>
2013-12-11 16:46:34 +0800

    No message.

--- app/models/topic_tag.rb
+++ app/models/topic_tag.rb
@@ -1,4 +1,7 @@
 class TopicTag < ActiveRecord::Base
   belongs_to :topic
   belongs_to :tag
+  validates_presence_of :topic_id, message: '帖子ID 不能为空'
+  validates_presence_of :tag_id,   message: '标签ID 不能为空'
+  validates_uniqueness_of :tag_id, scope: :topic_id, message: '帖子ID+标签ID 不能重复'
 end
